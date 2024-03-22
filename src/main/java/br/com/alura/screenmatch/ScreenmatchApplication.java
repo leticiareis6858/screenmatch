a04eaa669bb331466838f0a6c9d1d8c77c23c415
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-21 22:46:09 -0300

    feat: configura o uso dos metodos da classe 'Principal' na classe 'ScreenmatchApplication'

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI=new ConsumoAPI();
-		var json=consumoAPI.obterDados("https://www.omdbapi.com/?t=doctor+who&apikey=194f8ef8");
-		System.out.println(json);
+		Principal principal=new Principal();
+		principal.exibeMenu();
 
-		ConverteDados conversor=new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
 	}
 }
