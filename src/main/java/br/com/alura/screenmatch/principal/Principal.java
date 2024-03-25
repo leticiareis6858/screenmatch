4ce780d1e48e7c90af91dbb59b9a64d10cc48a67
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-25 01:15:09 -0300

    feat: adiciona coleta de estatítica susando Double Summary Statistics

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -112,5 +112,13 @@ public class Principal {
 
         System.out.println(avaliacaoPorTemporada);
 
+        DoubleSummaryStatistics est=episodios.stream()
+                .filter(e->e.getAvaliacao()>0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: "+est.getAverage());
+        System.out.println("Melhor episódio: "+est.getMax());
+        System.out.println("Pior episódio: "+est.getMin());
+        System.out.println("Quantidade: "+est.getCount());
     }
 }
