044914d353ee8cd1acabea43bfccceeafbf097f7
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-31 02:37:54 -0300

    feat: cria e implementa o 'SerieDTO'

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,29 @@
 package br.com.alura.screenmatch.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.dto.SerieDTO;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Listagem de séries";
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                        s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
+                        s.getSinopse()))
+                .collect(Collectors.toList());
     }
 
 }
