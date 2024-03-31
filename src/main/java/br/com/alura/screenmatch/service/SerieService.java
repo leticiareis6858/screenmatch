f51f369ab2f5e863d9865568685dd45b4fb33707
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-31 18:33:40 -0300

    feat: cria e implementa o método 'converteDados'

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
 @Service
@@ -16,17 +17,17 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-                        s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
-                        s.getSinopse()))
-                .collect(Collectors.toList());
+        return converteDados(repositorio.findAll());
+
     }
 
     public List<SerieDTO> obterTop5Series() {
-        return repositorio.seriesTop5()
-                .stream()
+        return converteDados(repositorio.seriesTop5());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
                         s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
                         s.getSinopse()))
