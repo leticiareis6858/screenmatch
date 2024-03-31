98b3220d77f0ba1659dc07c8e9d611b205b74dab
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-31 18:34:15 -0300

    feat: define '/series' como sendo o endpoint padrão

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,23 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.screenmatch.service.SerieService;
 import br.com.alura.screenmatch.dto.SerieDTO;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
-    @GetMapping("/series")
+    @GetMapping()
     public List<SerieDTO> obterSeries() {
         return service.obterTodasAsSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obterTop5Series() {
         return service.obterTop5Series();
     }
